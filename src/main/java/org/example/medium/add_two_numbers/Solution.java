9b467ec762b666ea22c64f11caf17464f493c63c
Eugene Kos <mstrsid@live.com>
2022-10-15 17:43:37 +0300

    task two numbers done

--- src/main/java/org/example/medium/add_two_numbers/Solution.java
+++ src/main/java/org/example/medium/add_two_numbers/Solution.java
@@ -1,7 +1,40 @@
 package org.example.medium.add_two_numbers;
 
-class Solution {
+import java.math.BigInteger;
+
+public class Solution {
+
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    return new ListNode();
+    ListNode node = null;
+    StringBuilder s1 = new StringBuilder();
+    StringBuilder s2 = new StringBuilder();
+    StringBuilder sum = new StringBuilder();
+
+    while (l1 != null) {
+      s1.append(l1.val);
+      l1 = l1.next;
+    }
+
+    while (l2 != null) {
+      s2.append(l2.val);
+      l2 = l2.next;
+    }
+
+    BigInteger res1 = new BigInteger(String.valueOf(s1.reverse()));
+    BigInteger res2 = new BigInteger(String.valueOf(s2.reverse()));
+    BigInteger bigSum = res1.add(res2);
+
+    sum.append(bigSum);
+
+    int i = 0;
+    while (i < sum.length()) {
+      if (node == null) {
+        node = new ListNode(Integer.parseInt(String.valueOf(sum.charAt(i))), null);
+      } else {
+        node = new ListNode(Integer.parseInt(String.valueOf(sum.charAt(i))), node);
+      }
+      i++;
+    }
+    return node;
   }
 }
